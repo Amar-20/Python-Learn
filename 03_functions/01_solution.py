e70b21daa88bf0e95566563e51f703f637868f2e
Amar-20 <konangeri1720@gmail.com>
2025-02-20 13:59:26 -0500

    functions

---
+++ 03_functions/01_solution.py
@@ -0,0 +1,9 @@
+# write a function to calculate and return square of a number.?
+
+number = int(input('Enter your number:'))
+
+def sqr_of_number(number):
+    return number**2
+
+result = sqr_of_number(number)
+print(result)
